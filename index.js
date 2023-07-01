b29603f610ffdfbfc90c99d1e420cc60952d8fcf
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-07-01 18:16:52 +0530

    completed the script and resolved all errors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import openai from './config/open_ai.js'
 import readlineSync from 'readline-sync';
 import colors from 'colors';
 
-const main = () => {
+const main = async() => {
     console.log(colors.bold.bgGreen.white('TERMICHAT WELCOMES YOU!'))
     console.log(colors.bold.green('Termichat is a chatbot that can talk to you about anything.'))
     console.log(colors.bold.green('It is powered by OpenAI\'s GPT-3 API.'))
@@ -10,16 +10,47 @@ const main = () => {
 
     let messagesSession = []
     console.log('\n\n')
+
     const name = readlineSync.question(colors.bold.yellow('What is your name? '))
+    messagesSession.push(['assistant', 'What is your name?'])
+    messagesSession.push(['user', name])
+
+    console.log()
     console.log(colors.bold.bgYellow.white('Termichat: ') + colors.bold.green(`Hello ${name}!`))
+    messagesSession.push(['assistant', `Hello ${name}!`])
+
     while(true) {
-        const prompt = readlineSync.question(colors.bold.bgYellow.white('You: '))
+        console.log()
+        const prompt = readlineSync.question(colors.bold.yellow('You: ')+' ')
         if(prompt.toLowerCase() === 'exit') {
-            console.log(colors.bold.bgYellow.white('Termichat: ') + colors.bold.green(`Bye ${name}!`))
+            console.log()
+            console.log(colors.bold.bgYellow.white('Termichat: ')+ ' ' + colors.bold.green(`Bye ${name}!`))
             break
         }
 
-        
+        try{
+            const messages = messagesSession.map((msg) => ({
+                role: msg[0],
+                content: msg[1]
+            }))
+
+            messages.push({ role: 'user', content: prompt })
+
+            const response = await openai.createChatCompletion({
+                model: 'gpt-3.5-turbo',
+                messages: messages,
+            })
+            console.log()
+            const answer = response.data.choices[0].message.content
+            console.log(colors.bold.green('Termichat: ')+ ' ' + answer)
+
+            messagesSession.push(['user', prompt])
+            messagesSession.push(['assistant', answer])
+        }
+        catch(err) {
+            console.log()
+            console.log(colors.bold.bgRed.white('Termichat: ') + colors.bold.red(err))
+        }
     }
 }
 
